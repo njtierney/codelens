ba77f4fd1e050c890f58ad171e0be325e66cf465
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-06 20:11:32 +0800

    first pass at getting the xml parser function detection and writing to work

--- inst/xml-parse-function.R
+++ inst/xml-parse-function.R
@@ -1,38 +1,31 @@
 library(xmlparsedata)
-
-p <- parse(text = readLines("inst/test-fun.R"), keep.source = TRUE)
-
 library(xml2)
-
-xml <- read_xml(xml_parse_data(p, pretty = TRUE))
+library(readr)
+library(tibble)
 
 xml_function <- function(xml){
   xp_fun <- paste0(
     "//expr",
     "[FUNCTION]"
   )
-  xml_find_all(xml, xp_fun)
+  xml2::xml_find_all(xml, xp_fun)
 }
 
 xml_line1 <- function(xml){
-  readr::parse_number(xml %>% xml_attr("line1"))
+  xml %>% xml2::xml_attr("line1") %>% readr::parse_number()
 }
 
 xml_line2 <- function(xml){
-  readr::parse_number(xml %>% xml_attr("line2"))
+  xml %>% xml2::xml_attr("line2") %>% readr::parse_number()
 }
 
-# extract_function_lines <-
-fun_line1 <- xml_function(xml) %>% xml_line1()
-fun_line2 <- xml_function(xml) %>% xml_line2()
+parse_as_xml <- function(text){
+  parse(text = text, keep.source = TRUE) %>%
+    xmlparsedata::xml_parse_data(pretty = TRUE)  %>%
+    xml2::read_xml()
+}
 
-tibble::tibble(start = fun_line1,
-               end = fun_line2) %>%
-  tibble::rowid_to_column(var = "fun")
-
-xml_function_start_end <- function(path){
-
-  xml <- read_xml(xml_parse_data(p, pretty = TRUE))
+xml_function_start_end <- function(xml){
 
   # extract_function_lines <-
   fun_line1 <- xml_function(xml) %>% xml_line1()
@@ -42,29 +35,66 @@ xml_function_start_end <- function(path){
                  end = fun_line2)
 }
 
-fun_start_end_pos <- xml_function_start_end("inst/test-fun.R")
-
-fun_start_end_pos
-
-test_fun_code <- readLines("inst/test-fun.R")
-
-mat <- t(fun_start_end_pos)
-
-split_mat <- function(x) split(x, rep(1:ncol(x), each = nrow(x)))
-
-fun_pos <- unname(split_mat(mat))
-
+split_mat <- function(x){
+  split(x, rep(1:ncol(x), each = nrow(x)))
+}
 seq_expand <- function(vec){
   seq(vec[1],  vec[2])
 }
 
-fun_pos_seq <- lapply(fun_pos, seq_expand)
+xml_symbol <- function(xml){
+  xp_symbol <- paste0(
+    "//expr",
+    "[SYMBOL]"
+  )
+  xml2::xml_find_all(xml, xp_symbol)
+}
 
-fun1 <- vctrs::vec_slice(test_fun_code, fun_pos_seq[[1]])
-readr::write_lines(x = fun1, file = "fun1.R")
+sym_start <- function(xml){
+  xml_symbol(xml) %>% xml2::xml_attr("col1") %>% readr::parse_number()
+}
 
-fun2 <- vctrs::vec_slice(test_fun_code, fun_pos_seq[[2]])
-readr::write_lines(x = fun2, file = "fun2.R")
+sym_end <- function(xml){
+  xml_symbol(xml) %>% xml2::xml_attr("col2") %>% readr::parse_number()
+}
 
-# now just need to get the function names
-# and also test against nested functions
+fun_name <- function(fun_text){
+  xml <- parse_as_xml(fun_text)
+  substr(fun_text[1], sym_start(xml), sym_end(xml))
+}
+
+extract_functions <- function(path) {
+  fun_code_text <- readLines(path)
+  xml <- parse_as_xml(fun_code_text)
+  fun_start_end_pos <- xml_function_start_end(xml)
+  mat <- t(fun_start_end_pos)
+  fun_pos <- unname(split_mat(mat))
+  fun_pos_seq <- lapply(fun_pos, seq_expand)
+  extracted_funs <- purrr::map(fun_pos_seq,
+                               ~vctrs::vec_slice(x = fun_code_text, i = .x))
+  fun_names <- purrr::map(extracted_funs, fun_name)
+  names(extracted_funs) <- fun_names
+  extracted_funs
+}
+
+write_functions <- function(extracted_functions, path = NULL){
+  file_names <- names(extracted_functions)
+  file_paths <- glue::glue('{path}{file_names}.R')
+  purrr::walk2(
+    .x = extracted_functions,
+    .y = file_paths,
+    .f = readr::write_lines
+  )
+}
+
+extract_functions("inst/test-fun.R")
+
+extract_functions("inst/test-fun.R") %>%
+  write_functions(path = "R/")
+
+# and now I can use this to ... write the functions out?!
+extract_functions("inst/xml-parse-function.R")
+
+extract_functions("inst/xml-parse-function.R") %>%
+  write_functions(path = "R/")
+# Need to test against nested functions
