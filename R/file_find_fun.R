160de9f99ff71de44acc35df50ac1b8d9d521275
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-08-10 15:01:25 +0800

    functions to find functions are exported

--- R/file_find_fun.R
+++ R/file_find_fun.R
@@ -1,9 +1,16 @@
+#' Find which files call a given function
+#'
+#' @param file file path
+#' @param fun function name like "source" or "library"
+#'
+#' @return dataframe where functions are called
+#' @export
 file_find_fun <- function(file, fun){
 
   cf <- readr::read_lines(file)
 
   cf_no_fun <- cf[stringr::str_detect(cf, paste0(fun, "\\("))] %>%
-    stringr::str_remove_all(paste0(fun, "\\(","|","\\)") %>%
+    stringr::str_remove_all(paste0(fun, "\\(","|","\\)")) %>%
     stringr::str_remove_all('\"')
 
   tibble::tibble(
--- R/file_sources.R
+++ R/file_sources.R
@@ -1,3 +1,9 @@
+#' Find where `library()` is called
+#'
+#' @param file file path
+#'
+#' @return dataframe of places where "library()" is called
+#' @export
 file_library <- function(file){
 
   file_find_fun(file, "library")
